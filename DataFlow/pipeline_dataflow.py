179a9c640d9a8c87a0fc50fbc55e4f5e5617fd4e
e-wach <e_wangting@hotmail.com>
2025-02-09 10:38:08 +0100

    Correción pipeline
    clase Firestore, eliminado el mode por ahora

--- DataFlow/pipeline_dataflow.py
+++ DataFlow/pipeline_dataflow.py
@@ -26,6 +26,32 @@ def ParsePubSubMessage2(message):
 
     return msg['categoria'], msg
 
+
+# Store in Firestore
+class StoreFirestoreDocument(beam.DoFn):
+
+    def __init__(self,firestore_collection):
+        self.firestore_collection = firestore_collection
+
+    def process(self, element):
+        db = firestore.Client()
+        events = ['volunteer', 'request']
+        category, (volunteer_data, request_data) = element
+
+        for event in events:
+            data_list = volunteer_data if event == 'volunteer' else request_data
+
+            if data_list:
+                for record in data_list:
+                    try:
+                        doc_id = record.get('created_at', '')
+                        if doc_id:
+                            db.collection(self.firestore_collection).document('not_matched').collection(event).document(doc_id).set(record)
+                            # logging.info(f"Stored {event} record in Firestore")
+                    except Exception as err:
+                        logging.error(f"Error storing {event}: {err}")
+
+
 # Filter by distance
 class FilterbyDistance(beam.DoFn):
 
@@ -63,53 +89,26 @@ class FilterbyDistance(beam.DoFn):
                 
                 if distance <= radio_max:
                     yield data
-
-# Store in Firestore
-class StoreFirestoreDocument(beam.DoFn):
-
-    def __init__(self,mode,firestore_collection):
-        self.mode = mode
-        self.firestore_collection = firestore_collection
-
-    def process(self, element):
-        db = firestore.Client()
-        events = ['volunteer', 'request']
-        category, (volunteer_data, request_data) = element
-
-        events = ['volunteer','request'] if self.mode == 'raw' else ['volunteer','request','distance']
-
-        for event in events:
-            data_list = volunteer_data if event == 'volunteer' else request_data
-
-            if data_list:
-                for record in data_list:
-                    try:
-                        doc_id = record.get('created_at', '')
-                        if doc_id:
-                            db.collection(self.firestore_collection).document('not_matched').collection(event).document(doc_id).set(record)
-                            logging.info(f"Stored {event} record in Firestore: {doc_id}")
-                    except Exception as err:
-                        logging.error(f"Error storing {event}: {err}")
-    
+                    logging.info(f"Match found: {data}")  
 
 # MATCH STATUS
-class MatchedStatusDoFn(beam.DoFn):
-    def process(self, element):
-        category, (help_data, volunteer_data) = element
+# class MatchedStatusDoFn(beam.DoFn):
+#     def process(self, element):
+#         category, (help_data, volunteer_data) = element
         
-        distance = help_data.get('distance')
+#         distance = help_data.get('distance')
 
-        matched_data = {
-            "category": category,
-            "volunteer": volunteer_data,
-            "request": help_data,
-            "distance": distance
-        }
+#         matched_data = {
+#             "category": category,
+#             "volunteer": volunteer_data,
+#             "request": help_data,
+#             "distance": distance
+#         }
 
-        if volunteer_data["categoria"] == help_data["etiqueta"] and distance <= volunteer_data["radio_disponible_km"]:
-            yield beam.pvalue.TaggedOutput("matched_users", matched_data)
-        else:
-            yield beam.pvalue.TaggedOutput("not_matched_users", matched_data)
+#         if volunteer_data["categoria"] == help_data["etiqueta"] and distance <= volunteer_data["radio_disponible_km"]:
+#             yield beam.pvalue.TaggedOutput("matched_users", matched_data)
+#         else:
+#             yield beam.pvalue.TaggedOutput("not_matched_users", matched_data)
 
 
 def run():
@@ -177,17 +176,20 @@ def run():
         category_grouped, category_not_grouped = (
             grouped_data | "Partition by volunteer found" >> beam.Partition(lambda kv, _: 0 if len(kv[1][0]) and len(kv[1][1]) > 0 else 1, 2)
         )
+
+        category_grouped | "Debug grouped data" >> beam.Map(lambda x: logging.info(f"Grouped data: {x}")) 
+
         # Partition 2: not grouped by category = send to firestore
         send_not_grouped = (
             category_not_grouped
             | "Send not grouped by category messages to Firestore" >> beam.ParDo(StoreFirestoreDocument(firestore_collection=args.firestore_collection))
         )
         # Partition 1: continues the Pipeline
-        # Filter by distance
-        # filtered_data = ( 
-        #     category_grouped
-        #     | "Filter by distance" >> beam.ParDo(FilterbyDistance())
-        # )
+        # Match by distance
+        filtered_data = ( 
+            category_grouped
+            | "Filter by distance" >> beam.ParDo(FilterbyDistance())
+        )
         
         # processed_data = (
         #     filtered_data
